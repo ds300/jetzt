4f04b9c8223e85712914698500f8df97c2c46f51
Ian Zapolsky <ianzapolsky@gmail.com>
2014-03-09 19:17:29 -0400

    removed all files to copy over from cloned workspace

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('Speed-reading in ' + tab.url);
-  chrome.tabs.executeScript(null,{
-    code: 'window.jetzt.select()'
-  });
-});

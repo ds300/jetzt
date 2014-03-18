2c651a0dbef6a94962d8935d143092fa908d2083
peteruithoven <peter@peteruithoven.nl>
2014-03-18 12:07:19 +0100

    Slightly more consistent whitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,35 +3,34 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-      banner: '/*! jetzt '
-        + '<%= grunt.template.today("yyyy-mm-dd") %>\n'
-        + '* https://github.com/ds300/jetzt/\n'
-        + '* Copyright (c) <%= grunt.template.today("yyyy") %> '
-        + 'David Sheldrick and contributors; Licensed Apache 2.0 */\n'
+    banner: '/*! jetzt '
+      + '<%= grunt.template.today("yyyy-mm-dd") %>\n'
+      + '* https://github.com/ds300/jetzt/\n'
+      + '* Copyright (c) <%= grunt.template.today("yyyy") %> '
+      + 'David Sheldrick and contributors; Licensed Apache 2.0 */\n'
     // Task configuration.
     , concat: {
-          options: {
-              banner: '<%= banner %>'
-            , stripBanners: true
-          }
-        , 'jetzt-solid.js' : [
-              "modules/preamble.js"
-            , "modules/helpers.js"
-            , "modules/config.js"
-            , "modules/parse.js"
-            , "modules/exec.js"
-            , "modules/view.js"
-            , "modules/select.js"
-            , "modules/control.js"
-            , "modules/init.js"
-          ]
+      options: {
+        banner: '<%= banner %>'
+        , stripBanners: true
       }
-
+      , 'jetzt-solid.js' : [
+            "modules/preamble.js"
+          , "modules/helpers.js"
+          , "modules/config.js"
+          , "modules/parse.js"
+          , "modules/exec.js"
+          , "modules/view.js"
+          , "modules/select.js"
+          , "modules/control.js"
+          , "modules/init.js"
+        ]
+      }
     , uglify: {
-          options: {
-            banner: '<%= banner %>'
-          }       
-        , 'jetzt-solid.min.js' : 'jetzt-solid.js'
+      options: {
+        banner: '<%= banner %>'
+      }       
+    	, 'jetzt-solid.min.js' : 'jetzt-solid.js'
     }
   });
 

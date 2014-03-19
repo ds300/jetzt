5deca133616d27df9ead02ccb88da865a50e70b4
David Sheldrick <djsheldrick@gmail.com>
2014-03-19 22:24:42 +0000

    simplified things a bit

--- modules/config.js
+++ modules/config.js
@@ -64,26 +64,26 @@
   var DEFAULT_OPTIONS = {
     // if we change config structure in future versions, having this means
     // we can update users' persisted configs to match.
-    config_version: CONFIG_VERSION
-  , target_wpm: 400
-  , scale: 1
-  , dark: false
-  , show_message: false
-  , selection_color: "#FF0000"
-  , modifiers: {
-      normal: 1
-    , start_clause: 1
-    , end_clause: 1.8
-    , start_sentence: 1.3
-    , end_sentence: 2.2
-    , start_paragraph: 2.0
-    , end_paragraph: 2.8
-    , short_space: 1.5
-    , long_space: 2.2
-    }
-  , font_family: "Menlo, Monaco, Consolas, monospace"
-  , selected_theme: 0
-  , custom_themes: []
+    config_version: CONFIG_VERSION,
+    target_wpm: 400,
+    scale: 1,
+    dark: false,
+    show_message: false,
+    selection_color: "#FF0000",
+    modifiers: {
+      normal: 1,
+      start_clause: 1,
+      end_clause: 1.8,
+      start_sentence: 1.3,
+      end_sentence: 2.2,
+      start_paragraph: 2.0,
+      end_paragraph: 2.8,
+      short_space: 1.5,
+      long_space: 2.2
+    },
+    font_family: "Menlo, Monaco, Consolas, monospace",
+    selected_theme: 0,
+    custom_themes: []
   };
 
   /*
@@ -116,44 +116,38 @@
 
 
   // helper function for creating getter functions
-  function getter (obj, prop, transform) {
-    if (transform)
-      return function () { return transform(obj[prop]); };
-    else
-      return function () { return obj[prop]; };
+  function getter (obj, prop) {
+    return function () { return obj[prop]; };
   }
 
   // and for setters
-  function setter (obj, prop, transform) {
-    if (transform)
-      return function (val) { obj[prop] = transform(val); };
-    else
-      return function (val) { obj[prop] = val; }
+  function setter (obj, prop) {
+    return function (val) { obj[prop] = val; }
   }
 
   // helper for making sure assigned numbers are within some range
-  function clamper (obj, prop, min, max) {
-    return setter(obj, prop, function (val) {
+  function clamper (setter, min, max) {
+    return function (val) {
       if (typeof val !== 'number') throw new Error("Expecting number");
-      return; H.clamp(min, val, max);
-    });
+      setter(H.clamp(min, val, max));
+    }
   }
 
   // and for making sure things are boolean
-  function bool (obj, prop) {
-    return setter(obj, prop, function (val) {
-      return !!val;
-    });
+  function bool (setter) {
+    return function (val) {
+      setter(!!val);
+    }
   }
 
   // colors need to be in hex form for the color picker to show them.
-  function color (obj, prop) {
-    return setter(obj, prop, function (val) {
+  function color (setter) {
+    return function (val) {
       if (!(typeof val === 'string') || !val.match(/^#[0-9a-fA-F]{6}$/))
         throw new Error("bad color format");
       else
-        return val;
-    });
+        setter(val);
+    }
   }
 
   // some things shouldn't be changed
@@ -161,6 +155,18 @@
     return function () {
       throw new Error("unmodifiable property");
     }
+  }
+
+  // silly javascript not having built-in function composition
+  function comp (f1, f2) {
+    return function () {
+      return f1(f2.apply(this, arguments));
+    };
+  }
+
+  // sometimes you need a function that always returns the same value
+  function constantly (val) {
+    return function () { return val; };
   }
 
   // this takes a flat array of [property name, getter, setter] tuples
@@ -171,60 +177,79 @@
   // that any time a variable is changed, the onchange event is triggered.
   function makeGettersSetters (obj, list, mutatorWrapper) {
     for (var i=0; i<list.length; i += 3) {
-      // list[i:i+3] is structured [property name, getter, setter]
+      // remember, list[i:i+3] is structured [property name, getter, setter]
       obj.__defineGetter__(list[i], list[i+1]);
       obj.__defineSetter__(list[i], mutatorWrapper(list[i+2]));
     }
   }
 
-  // front end for delay modifiers. has the same structure as in the internal
-  // representation, but with an additional `.list` method which returns the
-  // names of the modifiers.
-  function ModifiersFrontend (modifiers, mutatorWrapper) {
-    var defaults = DEFAULT_OPTIONS.modifiers;
-    var getset = [];
-    H.keys(defaults).forEach(function (key) {
-      getset.push(key, setter(modifiers, key), clamper(modifiers, key, 0, 5));
-    });
-    makeGettersSetters(this, getset, mutatorWrapper);
-    this.list = function () { return H.keys(defaults); }
-  }
-
-  // front end for individual themes. Again, same structure as IR but with
-  // a `.listColors` method.
-  function ThemeFrontend (theme, mutatorWrapper) {
-    this.listColors = function () { return H.keys(theme.colors); };
-
-    var colors = {};
-    makeGettersSetters(
-      colors,
-      H.flatten(H.keys(theme.colors).map(function (name) {
-        return [name, getter(theme.colors, name), color(theme.colors, name)];
+  // This takes an arbitrary object with uniform value types and creates
+  // a frontend for it using mutatorWrapper to wrap the setter methods.
+  function UniformFrontend (obj, mutatorWrapper) {
+    makeGettersSetters(this,
+      H.flatten(H.keys(obj).map(function (property) {
+        return [
+          property,
+          getter(obj, property),
+          setter(obj, property)
+        ];
       })),
       mutatorWrapper
     );
-
-    makeGettersSetters(
-      this,
-      [
-        "backdrop_opacity"
-      , getter(theme, "backdrop_opacity")
-      , color(theme, "backdrop_opacity")
-
-        "colors", function () { return colors; } , immuatble()
-      ],
-      mutatorWrapper);
   }
+
+  // the created object also has a .list convenience funtion.
+  UniformFrontend.prototype.list = function () { return H.keys(this); };
+
+
+  // front end for individual styles (a theme is composed of two styles: light
+  // and dark). Same structure as in IR
+  function StyleFrontend (style, mutatorWrapper) {
+    var colors = new UniformFrontend(style.colors, comp(mutatorWrapper, color));
+    makeGettersSetters(this, [
+      "backdrop_opacity",
+      getter(style, "backdrop_opacity"),
+      color(setter(style, "backdrop_opacity")),
+
+      "colors", constantly(colors), immuatble()
+
+    ], mutatorWrapper);
+  }
+
+  
 
   // front end for themes array. has a `.current` property which returns
   // the theme currently in use and can be set to any of the objects returned by
   // the `.list` method to change the theme. In addition, there's a `.next`
-  // method which cycles through the list of themes.
+  // method which cycles through the list of themes. Also a `newTheme` method
+  // for... making... new themes...
   function ThemesFrontend (opts, mutatorWrapper) {
     var themesArray;
 
-    function addRemoveMethod (customThemeFrontend) {
-      customThemeFrontend.remove = mutatorWrapper(function () {
+    // front end for individual themes. Again, same structure as IR but with
+    // a `.remove` method
+    function ThemeFrontend (theme, mutatorWrapper) {
+      var light = new StyleFrontend(theme.light, mutatorWrapper);
+      var dark = new StyleFrontend(theme.dark, mutatorWrapper);
+      makeGettersSetters(this, [
+        "name",
+        getter(theme, "name"),
+        function (name) {
+          if (!(typeof name === 'string')) throw new Error("Expecting string");
+          theme.name = name.trim() || "Custom Theme";
+        },
+    
+        "light", constantly(light), immuatble(),
+
+        "dark", constantly(dark), immuatble()
+
+      ], mutatorWrapper);
+    }
+
+    ThemeFrontend.prototype.remove = function () {
+      if (!this.isCustom()) {
+        throw new Error("Can't delete default themes")
+      } else {
         var idx = themesArray.indexOf(this);
         var optsIdx = idx - DEFAULT_THEMES.length;
         themesArray.splice(idx, 1);
@@ -232,76 +257,79 @@
         if (idx === themesArray.length) {
           opts.selected_theme--;
         }
-      });
-      return customThemeFrontend;
-    }
+      }
+    };
 
-    var themesArray = DEFAULT_THEMES.map(function (defaultTheme) {
+    ThemeFrontend.prototype.isCustom = function () {
+      return themesArray.indexOf(this) >= DEFAULT_THEMES.length;
+    };
+
+    themesArray = DEFAULT_THEMES.map(function (defaultTheme) {
       return new ThemeFrontend(defaultTheme, immuatble);
     }).concat(opts.custom_themes.map(function (customTheme) {
-      return addRemoveMethod(new ThemeFrontend(customTheme, mutatorWrapper));
+      return new ThemeFrontend(customTheme, mutatorWrapper);
     }));
 
-    this.new = mutatorWrapper(function (select) {
+    this.next = mutatorWrapper(function () {
+      opts.selected_theme = (opts.selected_theme + 1) % themesArray.length;
+    });
+
+    this.newTheme = mutatorWrapper(function (select) {
       var newTheme = JSON.parse(JSON.stringify(DEFAULT_THEMES[0]));
       opts.custom_themes.push(newTheme);
-      themesArray.push(
-        addRemoveMethod(new ThemeFrontend(newTheme, mutatorWrapper)
+      var frontend = addRemoveMethod(
+        new ThemeFrontend(newTheme, mutatorWrapper)
       );
+      themesArray.push(frontend);
       if (select) {
         opts.selected_theme = themesArray.length - 1;
       }
+      return frontend;
     });
 
     this.list = function () { return themesArray.slice(0); };
 
     var getset = [
-      "current"
-    , function () { return themesArray[opts.selected_theme]; }
-    , function (theme) {
+      "current",
+      function () { return themesArray[opts.selected_theme]; },
+      function (theme) {
         var idx = themes.indexOf(theme);
         if (idx > -1) {
           options.selected_theme = idx;
         } else {
-          themes.newTheme(true);
-          this.theme = H.recursiveExtend(this.theme, theme);
+          this.newTheme(true);
+          H.recursiveExtend(this.theme, theme);
         }
       }
     ];
+    makeGettersSetters(this, getset, mutatorWrapper);
   }
 
+  // front end for entire jetzt config.
   function ConfigFrontend (opts, mutatorWrapper) {
-    var modifiers = new ModifiersFrontend(opts.modifiers, mutatorWrapper);
-    var themes = new ThemesFrontend(opts);
+    var modifiers = new UniformFrontend(opts.modifiers, function (setter) {
+      return mutatorWrapper(clamper(setter, 0, 5));
+    });
+    var themes = new ThemesFrontend(opts, mutatorWrapper);
     var get = function (prop) { return getter(opts, prop); }
+    var set = function (prop) { return setter(opts, prop); }
 
     var getset = [
-      "scale", get("scale"), clamper(opts, "scale", 0.1, 10)
+      "scale", get("scale"), clamper(set("scale"), 0.1, 10)
 
-    , "dark", get("dark"), bool(opts, "dark")
+      "dark", get("dark"), bool(set("scale")),
 
-    , "wpm", get("target_wpm"), clamper(opts, "target_wpm", 100, 1500)
+      "wpm", get("target_wpm"), clamper(set("target_wpm"), 100, 1500),
 
-    , "selection_color", get("selection_color"), color(opts, "selection_color")
+      "selection_color", get("selection_color"), color(set("selection_color")),
 
-    , "show_message", get("show_message"), bool(opts, "show_message")
+      "show_message", get("show_message"), bool(opts, "show_message"),
 
-    , "font", get("font_family"), setter(opts, "font_family")
+      "font", get("font_family"), set("font_family"),
 
-    , "theme"
-    , function () { return themes[opts.selected_theme]; }
-    , function (theme) {
-        var idx = themes.indexOf(theme);
-        if (idx > -1) {
-          options.selected_theme = idx;
-        } else {
-          themes.newTheme(true);
-          this.theme = H.recursiveExtend(this.theme, theme);
-        }
-      }
+      "modifiers", constantly(modifiers) , immuatble(),
 
-    , "modifiers", function () { return modifiers; } , immuatble()
-    , "themes", function () { return themes; }, immuatble()
+      "themes", constantly(themes), immuatble()
     ];
 
     makeGettersSetters(this, getset, mutatorWrapper);
@@ -370,16 +398,16 @@
    * void get(cb(opts))
    * void set(opts)
    */
-  config.setBackend = function (backend) {
+  jetzt.config.setBackend = function (backend) {
     configBackend = backend;
-    config.refresh();
+    this.refresh();
     announce();
   };
 
   /**
    * Triggers an automatic reload of the persisted options
    */
-  config.refresh = function (cb) {
+  jetzt.config.refresh = function (cb) {
     configBackend.get(function (json) {
       unpersist(json);
       cb && cb();
@@ -422,12 +450,12 @@
   /**
    * convenience function for finding the highest of two modifiers.
    */
-  config.maxModifier = function (a, b) {
+  jetzt.config.maxModifier = function (a, b) {
     return this.modifiers[a] > this.modifiers[b] ? a : b;
   };
 
 
   // load the options from the default config backend to get the ball rolling
-  config.refresh();
+  jetzt.config.refresh();
 
 })(this);
